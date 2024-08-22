3ab0475525df64fbb7f5c4563d7ff12977b04d9f
shubham singh <singhshubham9893@gmail.com>
2024-08-22 12:25:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ document.addEventListener('keydown', function(event) {
     document.getElementById('key-box').textContent = keyCode;
 });
 
-
-// Create a new Audio object
-const sound = new Audio('sound.mp3');
-
-// Add an event listener to the document for keydown events
-document.addEventListener('keydown', () => {
-  // Play the sound when a key is pressed
-  sound.play();
-});
